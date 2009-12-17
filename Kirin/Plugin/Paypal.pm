ee035484484e479c5b1fc83a6196e392f09fee11
Simon Cozens <simon@simon-cozens.org>
2009-12-17 12:37:45 +0000

    Cancelling the payment.

--- Kirin/Plugin/Paypal.pm
+++ Kirin/Plugin/Paypal.pm
@@ -4,6 +4,20 @@ use Business::PayPal;
 
 sub _skip_auth { "ipn" }
 
+sub cancel { 
+    my ($self, $mm) = @_;
+    # Back to reviewing the invoice;
+    my $frob = $mm->{req}->parameters->{custom};
+    my ($pp) = Kirin::DB::Paypal->search(magic_frob => $frob);
+    if (!$pp) { # Something's gone weird, return them to their customer page
+        Kirin::Plugin::Customer->view($self, $mm);
+    }
+    my $invoice = $pp->invoice;
+    $pp->delete;
+    push @{$mm->{messages}}, "You cancelled the invoice payment";
+    $mm->respond("plugins/invoice/view", invoice => $invoice);
+}
+
 sub _pay_invoice {
     my ($self, $invoice, $mm) = @_;
     my $paypal = Business::PayPal->new;
@@ -31,5 +45,9 @@ sub _pay_invoice {
     $pp->magic_frob($paypal->id);
     $pp->update();
     return $button;
+}
+
+sub _setup_db {
+    Kirin::DB::Paypal->has_a(invoice => "Kirin::DB::Invoice");
 }
 1;
