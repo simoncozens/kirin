87c834e153ffc8691187c2a3faca6f05a33a5eda
Simon Cozens <simon@simon-cozens.org>
2009-12-23 17:35:03 +0000

    Some more options we need.

--- kirin.pl
+++ kirin.pl
@@ -1,6 +1,7 @@
 use Kirin;
 Kirin->app(
-    port => "1978",
     template_path => "templates",
-    dsn => "dbi:SQLite:kirin.db"
+    dsn => "dbi:SQLite:kirin.db",
+    base => "http://localhost:5000/",
+    paypal_recipient => 'simon@simon-cozens.org'
 );
