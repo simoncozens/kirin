d36edd1e217df0375b39e10780ef873af1e13a85
Simon Cozens <simon@simon-cozens.org>
2009-12-28 11:57:37 +0000

    Keep the domain name handy

--- ssl.sql
+++ ssl.sql
@@ -1,6 +1,7 @@
 CREATE TABLE ssl_certificate ( id integer primary key not null,
     customer integer,
     enom_cert_id integer,
+    domain varchar(255), /* We could parse the CSR but that's horrid */
     csr text,
     key_file text,
     certificate text,
