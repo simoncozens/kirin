556498f74db6ff08e54c8ace9cc0f946ac973e01
Simon Cozens <simon@simon-cozens.org>
2009-12-28 17:33:08 +0000

    It's a brave new world.

--- Kirin/Plugin/Domain.pm
+++ Kirin/Plugin/Domain.pm
@@ -4,14 +4,10 @@ use base 'Kirin::Plugin';
 
 sub user_name { "Domains"               } 
 
-sub handle {
-    my ($self, $req, @args) = @_;
-}
-
 sub list {
-    my ($self, $req, $action) = @_;
-    Kirin->respond($req, $action, "plugins/domainlist", 
-            domains => [ $req->{user}->my_domains($action) ]
+    my ($self, $mm) = @_;
+    $mm->respond("plugins/domain/list", 
+            domains => [ $req->{customer}->domains ]
     );
 }
 
