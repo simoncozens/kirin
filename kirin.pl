37cc0b3f48e12fa7ae30a498315089d3f2b473ca
Simon Cozens <simon@simon-cozens.org>
2009-12-28 11:58:05 +0000

    Test enom user details.

--- kirin.pl
+++ kirin.pl
@@ -3,5 +3,7 @@ Kirin->app(
     template_path => "templates",
     dsn => "dbi:SQLite:kirin.db",
     base => "http://localhost:5000/",
-    paypal_recipient => 'simon@simon-cozens.org'
+    paypal_recipient => 'simon@simon-cozens.org',
+    enom_reseller_username => "resellid",
+    enom_reseller_password => "resellpw"
 );
