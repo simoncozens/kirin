69e769bb721a0c9d6c3146bc2783f8462b2894ea
Simon Cozens <simon@simon-cozens.org>
2009-12-31 15:03:50 +0000

    Add domains, if we can and they're something to do with us.

--- Kirin/Plugin/Domain.pm
+++ Kirin/Plugin/Domain.pm
@@ -1,25 +1,59 @@
 package Kirin::Plugin::Domain;
 use strict;
 use base 'Kirin::Plugin';
+use Regexp::Common qw/dns/;
+
 sub default_action { "list" }
 
 sub user_name { "Domains"               } 
 
 sub list {
     my ($self, $mm) = @_;
-    # Add another hosted domain?
-    if ($mm->param("adding")) {
-        if (!$self->_can_add_more($mm->{customer})) {    # No can do
-        }
+    my $can_add = $self->_can_add_more($mm->{customer});
+    if ($mm->param("adding") and my $domain = $self->_validate($mm, $can_add)) {
+        Kirin::DB::Domain->create({
+            domainname => $domain, customer => $mm->{customer} 
+        });
+        $self->_add_todo($mm, addhosting => $domain);
+        $mm->message("Your domain has been added and will be available shortly.");
     }
     $mm->respond("plugins/domain/list", 
             domains => [ $mm->{customer}->domains ],
-            relations => [ $self->relations ]
+            relations => [ $self->relations ],
+            can_add_more => $can_add
     );
 }
 
+sub _validate {
+    my ($self, $mm, $can_add) = @_;
+    if (!$can_add) {
+        $mm->message("You cannot add any more domains to your account; please purchase more hosting packages");
+        return;
+    } 
+    my $dn = $mm->param("domainname") or return;
+    if (!$dn =~ $RE{'dns'}{'domain'}{-minlables => 2}) {
+        $mm->message("Domain name malformed"); return;
+    }
+
+    my $res = Net::DNS::Resolver->new;
+    my $query = $res->query($dn, "NS");
+    if (!$query) {
+        $mm->message("Domain '$dn' doesn't have a configured nameserver or isn't registered - do you need to register it first?");
+        return;
+    }
+    # Is it something to do with us?
+    if ($self->_is_hosted_by($dn => "MX", Kirin->args->{mx_server})
+     or $self->_is_hosted_by($dn => "NS", Kirin->args->{primary_dns_server})
+     or $self->_is_hosted_by("www.$dn" => "A", Kirin->args->{hosting_web_server})) {
+        return $dn;
+    }
+
+    $mm->message("I don't think we host anything for that domain. Email the administrator if you think this is in error.");
+    return;
+}
+
 sub _setup_db {
-    shift->_ensure_table("domain");
+shift->_ensure_table("domain");
     Kirin::DB::Domain->has_a(customer => "Kirin::DB::Customer");
     Kirin::DB::Customer->has_many(domains => "Kirin::DB::Domain");
 }
