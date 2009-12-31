f20dcab0254745f3310b198440c56a8e6d5774f4
Simon Cozens <simon@simon-cozens.org>
2009-12-31 16:56:56 +0000

    Ask for the primary nameserver.

--- Kirin/Plugin/SecondaryDns.pm
+++ Kirin/Plugin/SecondaryDns.pm
@@ -5,6 +5,7 @@ sub exposed_to     { 0 }
 sub name { "secondary_dns" }
 sub user_name      { "Backup DNS" }
 sub default_action { "edit" }
+use Regexp::Common qw/net/;
 
 Kirin::Plugin::SecondaryDns->relates_to("Kirin::Plugin::Domain");
 
@@ -17,11 +18,18 @@ sub edit {
     # Do we have one?
     my ($rec) = Kirin::DB::SecondaryDns->search(domain => $domain);
     if ($mm->param("editing") and $mm->param("on")) {
-        warn "Adding";
         if ($rec) {
         } elsif ($self->_can_add_more($mm->{customer})) {
-            $rec = Kirin::DB::SecondaryDns->create({ domain => $domain, customer => $mm->{customer}});
-            $self->_add_todo($mm, add_backup => $domain);
+            my $primary = $mm->param("primary_server");
+            if ($primary =~ /^$RE{net}{IPv4}$/) {
+                $rec = Kirin::DB::SecondaryDns->create({ 
+                    domain => $domain, customer => $mm->{customer}, 
+                    primary_server => $primary
+                });
+                $self->_add_todo($mm, add_backup => "$domain:$primary");
+            } else {
+                $mm->message("Primary server doesn't look like an IP address");
+            }
         } else { 
             $mm->message("You can't add any more backup MXes; do you need to purchase more services?");
         }
@@ -52,7 +60,8 @@ sub sql { q/
 CREATE TABLE IF NOT EXISTS secondary_dns (
     id integer primary key not null,
     customer integer,
-    domain integer
+    domain integer,
+    primary_server varchar(255)
 );
 / }
 
