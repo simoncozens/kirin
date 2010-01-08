84291814973d9d233ded2a8efe6f0ef4b11c6e2c
Simon Cozens <simon@simon-cozens.org>
2010-01-08 12:26:44 +0000

    Allow root to view and edit job list.

--- Kirin/Plugin/Jobqueue.pm
+++ Kirin/Plugin/Jobqueue.pm
@@ -2,10 +2,21 @@ package Kirin::Plugin::Jobqueue;
 use base 'Kirin::Plugin';
 sub exposed_to { $_[1]->is_root() }
 sub name { "jobqueue"; }
+sub default_action { "list" }
 sub list { # Oh why not
-    
+    my ($self, $mm) = @_;
+    if (!$mm->{user}->is_root) { return $mm->respond("403handler") }
+    if ($mm->param("deleting") and 
+        my $j = Kirin::DB::Jobqueue->retrieve($mm->param("jid"))) {
+        $j->delete; $mm->message("Job deleted");
+    }
+    return $mm->respond("plugins/jobqueue/list", 
+        jobs => [ Kirin::DB::Jobqueue->retrieve_all ]);
 }
 
+sub _setup_db {
+    Kirin::DB::Jobqueue->has_a(customer => "Kirin::DB::Customer");
+}
 package Kirin::DB::Jobqueue;
 
 sub process {
