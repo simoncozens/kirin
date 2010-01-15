888fbc66759c95a5b8289194a848458fc068572e
Simon Cozens <simon@simon-cozens.org>
2010-01-15 17:28:18 +0000

    Root user is user 1.

--- Kirin/Plugin/User.pm
+++ Kirin/Plugin/User.pm
@@ -35,36 +35,7 @@ sub my_customers {
 
 sub is_root {
     my $self = shift;
-    my $acl;
-    if (($acl) = Kirin::DB::Acl->search(user => $self->id, domain => "*", action => "*", yesno => 1)) { return 1 }
-}
-
-sub can_do { # Our simple ACL processor
-    my ($self, $action, $domain) = @_;
-    my $acl;
-    return 1 if $self->is_root;
-    return 1;
-    if (!$action) { # Just see if we can see this domain at all
-       return max map { $_->yesno }  Kirin::DB::Acl->search(user => $self, domain => $domain);
-    } 
-    if (!$domain) { 
-       return max map { $_->yesno }  Kirin::DB::Acl->search(user => $self, action => $action);
-    } 
-    if (($acl) = Kirin::DB::Acl->search(user => $self, domain => $domain, action => $action)) { return $acl->yesno; }
-    if (($acl) = Kirin::DB::Acl->search(user => $self, domain => "*", action => $action)) { return $acl->yesno; }
-    if (($acl) = Kirin::DB::Acl->search(user => $self, domain => $domain, action => "*")) { return $acl->yesno; }
-    if (($acl) = Kirin::DB::Acl->search(user => $self, domain => "*", action => "*")) { return $acl->yesno; }
-    return 0;
-}
-
-sub my_domains { # All the domains that I can X
-    my $self = shift;
-    my $action = shift;
-    if ($self->is_root) { return Kirin::DB::Domain->retrieve_all }
-    my @ok_domains;
-    return grep {$self->can_do($action, $_->domainname)}
-        map { $_->domains } 
-        $self->customers;
+    $self->id == 1; # For now   
 }
 
 1;
