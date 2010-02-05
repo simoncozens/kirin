d48dfe243f22b90e7a2a3c9fb83ee9ff735556f1
Simon Cozens <simon@simon-cozens.org>
2010-02-05 17:37:28 +0000

    It's starting to take shape.

---
+++ Kirin/Plugin/Broadband.pm
@@ -0,0 +1,123 @@
+package Kirin::Plugin::Broadband;
+use strict;
+use base 'Kirin::Plugin';
+use Net::DSLProvider;
+sub user_name      { "Broadband" }
+sub default_action { "view" }
+my $murphx;
+
+sub order {
+    my ($self, $mm) = @_;
+    if (my $clid = $mm->param("clid")) {
+        my %avail = $murphx->services_available($clid);
+    }
+}
+
+sub view {
+    my ($self, $mm) = @_;
+    # Do we have a service? If so, say something about it.
+    if (my $bb = $mm->{customer}->broadband) { 
+        # Get current bandwidth usage info - force update, and history 
+        # will be available to the template as broadband.usage_reports
+        $bb->get_current_bandwidth(undef,undef,1);
+        # Get any other status we care about
+        # XXX How do I find out how much bandwidth allowance we have?
+
+        return $mm->respond("plugins/broadband/currentstatus",
+            broadband => $bb,
+        );
+    } else {
+        return $mm->respond("plugins/broadband/signup");
+    }
+}
+
+sub _handle_cancel_request {
+    my ($self, $customer, $service) = @_;
+    # If we're out of databases, get someone to (carefully) delete them
+}
+
+sub _setup_db {
+    Kirin->args->{$_}
+        || die "You need to configure $_ in your Kirin configuration"
+        for qw/murphx_username murphx_password murphx_clientid/;
+    $murphx = Net::DSLProvider::Murphx->new({
+        user => Kirin->args->{murphx_username},
+        pass => Kirin->args->{murphx_password},
+        clientid => Kirin->args->{murphx_clientid}
+    });
+
+    shift->_ensure_table("broadband");
+    Kirin::DB::Broadband->has_a(customer => "Kirin::DB::Customer");
+    Kirin::DB::Customer->might_have(broadband => "Kirin::DB::Broadband");
+    Kirin::DB::BroadbandEvent->has_a(broadband => "Kirin::DB::Broadband");
+    Kirin::DB::Broadband->has_many(events => "Kirin::DB::BroadbandEvent");
+    Kirin::DB::BroadbandUsage->has_a(broadband => "Kirin::DB::Broadband");
+    Kirin::DB::Broadband->has_many(usage_reports => "Kirin::DB::BroadbandUsage");
+}
+
+package Kirin::DB::Broadband;
+
+sub provider_handle {
+    my $self = shift;
+    my $p = $self->provider;
+    my $module = "Net::DSL::Provider::".ucfirst($p);
+    $module->new({ 
+        user     => Kirin->args->{"${p}_username"},
+        pass     => Kirin->args->{"${p}_password"},
+        clientid => Kirin->args->{"${p}_clientid"},
+    });
+}
+
+sub get_bandwidth_for {
+    my ($self, $year, $mon, $replace) = @_;
+    $year ||= 1900 + (localtime)[5];
+    $mon  ||= 1    + (localtime)[4];
+    $mon = sprintf("%02d", $mon);
+    my $bw = $self->usage_reports(year => $year, mon => $mon);
+    if ($bw and !$replace) {
+        return ($bw->input, $bw->output);
+    }
+    my %summary = $self->provider_handle->usage_summary(
+        "service-id" => $self->token,
+        year => $year,
+        mon => $mon
+    );
+    if ($bw) { 
+        $bw->input($summary{"total-input-octets"});
+        $bw->output($summary{"total-output-octets"});
+    } else {
+        $self->add_to_usage_reports({
+            year => $year,
+            mon => $mon,
+            input => $summary{"total-input-octets"},
+            output => $summary{"total-output-octets"},
+        });
+    }
+    return ($summary{"total-input-octets"}, $summary{"total-output-octets"});
+}
+
+sub sql {q/
+CREATE TABLE IF NOT EXISTS broadband (
+    id integer primary key not null,
+    customer integer,
+    telno varchar(12),
+    provider varchar(255),
+    token varchar(255),
+    status varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS broadband_event (
+    id integer primary key not null,
+    broadband integer
+);
+
+CREATE TABLE IF NOT EXISTS broadband_bandwidth (
+    id integer primary key not null,
+    broadband integer,
+    year integer,
+    month integer,
+    input integer,
+    output integer    
+
+/}
+1;
