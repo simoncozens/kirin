619868e1f3c311c56510100b2080628619c60d3d
Simon Cozens <simon@simon-cozens.org>
2010-02-23 17:00:28 +0000

    Split backup MX and secondary DNS.

--- Kirin/Plugin/SecondaryDns.pm
+++ Kirin/Plugin/SecondaryDns.pm
@@ -15,35 +15,38 @@ sub edit {
     ($domain, $r) = Kirin::DB::Domain->web_retrieve($mm, $domain);
     return $r if $r;
 
-    # Do we have one?
     my ($rec) = Kirin::DB::SecondaryDns->search(domain => $domain);
-    if ($mm->param("editing") and $mm->param("on")) {
-        if ($rec) {
-        } elsif ($self->_can_add_more($mm->{customer})) {
-            my $primary = $mm->param("primary_server");
-            if ($primary =~ /^$RE{net}{IPv4}$/) {
-                $rec = Kirin::DB::SecondaryDns->create({ 
-                    domain => $domain, customer => $mm->{customer}, 
-                    primary_server => $primary
-                });
-                $self->_add_todo($mm, add_backup => "$domain:$primary");
-            } else {
-                $mm->message("Primary server doesn't look like an IP address");
-            }
-        } else { 
-            $mm->no_more("backup MXes");
-        }
-    } elsif ($mm->param("editing")) { # Turn it off
-        if ($rec) { 
-            $rec->delete;
-            $self->_add_todo($mm, remove_backup => $domain);
-            undef $rec;
-        }
-    }
+    $self->_process_form($mm, $domain, $rec) if $mm->param("editing");
+    $mm->respond("plugins/secondary_dns/edit", domain => $domain, have => $rec);
+}
 
-    $mm->respond("plugins/secondary_dns/edit", domain => $domain,
-        have_already => $rec
-    );
+sub _process_form {
+    my ($self, $mm, $domain, $rec) = @_;
+    my $on = ($mm->param("mx")||$mm->param("ns"));
+    my $primary = $mm->param("primary_server");
+    return if !$rec and !$on;
+    if (!$rec and $on and !$self->_can_add_more($mm->{customer})) {
+        $mm->no_more("backup DNS");
+        return;
+    } elsif ($mm->param("ns") and $primary !~ /^$RE{net}{IPv4}$/) {
+        $mm->message("Primary server doesn't look like an IP address");
+        return;
+    } 
+    if (!$rec) {
+        $rec = Kirin::DB::SecondaryDns->create({ 
+            domain => $domain, customer => $mm->{customer}, 
+            primary_server => $primary,
+            sdns => $mm->param("ns"),
+            mx   => $mm->param("mx")
+        });
+    } else {
+        $rec->sdns($mm->param("ns"));
+        $rec->mx($mm->param("mx"));
+        $rec->primary_server($primary);
+        $rec->update;
+    }
+    $self->_add_todo($mm, setup => $rec->id);
+
 }
 
 sub _setup_db {
@@ -61,6 +64,8 @@ CREATE TABLE IF NOT EXISTS secondary_dns (
     id integer primary key not null,
     customer integer,
     domain integer,
+    mx integer,
+    sdns integer,
     primary_server varchar(255)
 );
 / }
